1f47d54e5bdf5ed25def25497056b1fe08e85dce
Suryansh Anand <spirit@anandsur.com>
2024-02-04 13:42:25 +0530

    feat: neondb setup

--- db/index.ts
+++ db/index.ts
@@ -3,12 +3,18 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import * as schema from "./schema";
 
+const dbCredentials = process.env.DATABASE_URL
+  ? { connectionString: process.env.DATABASE_URL, ssl: true }
+  : {
+      host: "127.0.0.1",
+      port: 5432,
+      user: "postgres",
+      password: "root",
+      database: "postgres",
+    };
+
 const pool = new Pool({
-  host: "127.0.0.1",
-  port: 5432,
-  user: "postgres",
-  password: "root",
-  database: "postgres",
+  ...dbCredentials,
 });
 const db = drizzle(pool, { schema });
 
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,18 @@
 import type { Config } from "drizzle-kit";
 
+const dbCredentials = process.env.DATABASE_URL
+  ? { connectionString: process.env.DATABASE_URL, ssl: true }
+  : {
+      host: "127.0.0.1",
+      port: 5432,
+      user: "postgres",
+      password: "root",
+      database: "postgres",
+    };
+
 export default {
   schema: "./db/schema.ts",
   out: "./drizzle",
   driver: "pg",
-  dbCredentials: {
-    host: "127.0.0.1",
-    port: 5432,
-    user: "postgres",
-    password: "root",
-    database: "postgres",
-  },
+  dbCredentials,
 } satisfies Config;
