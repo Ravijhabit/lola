59674a7e9a8e2eb674f69c49c1320ddebc8ac492
Suryansh Anand <spirit@anandsur.com>
2024-02-04 10:46:14 +0530

    feat: reviwes have name and profile picture

--- app/items/[id]/Reviews.tsx
+++ app/items/[id]/Reviews.tsx
@@ -1,17 +1,37 @@
-import { SelectReviews } from "@/db/schema";
-import { FaUser } from "react-icons/fa";
+import db from "@/db";
+import { SelectReviews, users } from "@/db/schema";
+import { eq } from "drizzle-orm";
+import Image from "next/image";
 import styles from "./page.module.css";
-export default function Reviews({ reviews }: { reviews: SelectReviews[] }) {
+export default async function Reviews({
+  reviews,
+}: {
+  reviews: SelectReviews[];
+}) {
   return (
     <section className={styles.review}>
-      {reviews?.map((review) => (
-        <section key={review.id} className={styles.review}>
-          <FaUser />
-          {/* comment */}
-          <p>{review.rating}</p>
-          <p>{review.review}</p>
-        </section>
-      ))}
+      {reviews?.map(async (review) => {
+        const user = await db
+          .select()
+          .from(users)
+          .where(eq(users.id, review.buyer));
+
+        return (
+          <section key={review.id} className={styles.review}>
+            <Image
+              width={30}
+              height={30}
+              src={user[0].image}
+              alt={user[0].name}
+              className="rounded-full"
+            />
+            {user[0].name}
+            {/* comment */}
+            <p>{review.rating}</p>
+            <p>{review.review}</p>
+          </section>
+        );
+      })}
     </section>
   );
 }
